bc6a1bf5a289b3dc9dbeb64d46f1b95f94f1412f
Nataliia Dmytrenko <ednata82@gmail.com>
2022-07-31 22:21:04 +0300

    finished my awesome work

---
+++ src/featureNataliia/TestNataliia.java
@@ -0,0 +1,8 @@
+package featureNataliia;
+
+public class TestNataliia {
+
+    public static void main(String[] args) {
+        System.out.println("I added some good features to our project");
+    }
+}
